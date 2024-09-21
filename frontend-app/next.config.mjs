14725dfeca53aeb5af2e80dc58a7549ef5ccb638
Wanyonyi <g.nanguti@gmail.com>
2024-09-21 17:37:32 +0300

    add theme

--- frontend-app/next.config.mjs
+++ frontend-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"],
+    domains: ["images.pixels.com"],
+  },
+};
 
 export default nextConfig;
